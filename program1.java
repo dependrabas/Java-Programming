6b665c8e07078e3bce2a3f848295719803023c06
Dependra Basnet <126497750+dependrabas@users.noreply.github.com>
2024-01-10 19:48:04 +0530

    Create program1.java
    my First program

---
+++ program1.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.util.*;
+
+class First {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a, b, c;
+        System.out.println("Enter the Two Numbers:");
+        a = s.nextInt();
+        b = s.nextInt();
+        c = a + b;
+        System.out.println("Sum = " + c);
+
+    }
+}
